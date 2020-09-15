600ccb4e30ae608d6e7534b8243abd6657b4d51b
kvnsu <k.v.n.such@gmail.com>
2020-09-15 16:54:39 -0300

    Docker docker docker xD

--- java-container.sh
+++ java-container.sh
@@ -2,7 +2,7 @@ docker stop videoviz || true
 docker rm videoviz || true
 docker build -t videoviz/videoviz:$BUILD_ID . -f docker/java/Dockerfile || true
 docker run -d --name videoviz \
- -p 9011:8080 \
+ -p 8080:8080 \
  --env spring_profiles_active=prod \
  -v /var/www/html/gui:/var/www/html/gui \
  -v /var/www/html/v1/:/var/www/html/v1/ \
