eb4a91a47cca1f7be560621d725d793ca1b25a2b
kevim <k.v.n.such@gmail.com>
2020-09-15 02:02:56 -0300

    Script de iniciar no jenkins

--- java-container.sh
+++ java-container.sh
@@ -6,6 +6,6 @@ docker run -d --name videoviz \
  --env spring_profiles_active=prod \
  -v /var/www/html/gui:/var/www/html/gui \
  -v /var/www/html/v1/:/var/www/html/v1/ \
- -v /var/www/html/v2/:/var/www/html/v2/
- --restart=always
- \ videoviz/videoviz:$BUILD_ID || true
+ -v /var/www/html/v2/:/var/www/html/v2/ \
+ --restart=always \
+ videoviz/videoviz:$BUILD_ID || true
