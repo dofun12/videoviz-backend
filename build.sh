ccc0b6492f8b833e710e031c3f4e7193000eec75
kevim <k.v.n.such@gmail.com>
2020-12-30 18:31:34 -0300

    Nova versão

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mvn verify clean --fail-never
+mvn verify clean --fail-never -DskipTests
 mvn package -DskipTests
 touch VERSION.txt
 echo $(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout) > VERSION.txt
