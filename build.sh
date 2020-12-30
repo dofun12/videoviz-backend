e2bd042b840c82fe173ab4281065b2b7f12cf02a
kevim <k.v.n.such@gmail.com>
2020-12-30 01:50:23 -0300

    Corrigindo build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mvn clean package -DskipTests
+mvn verify clean --fail-never
+mvn package -DskipTests
 touch VERSION.txt
 echo $(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout) > VERSION.txt
