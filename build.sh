693815eaacd13634aaa8d9d527517460a59d2918
kevim <k.v.n.such@gmail.com>
2020-12-30 01:13:59 -0300

    Criando arquivo de versão

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mvn clean install package -DskipTests
+mvn clean package -DskipTests
 touch VERSION.txt
 echo $(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout) > VERSION.txt
