c56b15217c0f945482d030548b2ff7cdbfcc34b1
kevim <k.v.n.such@gmail.com>
2021-03-13 18:32:49 -0300

    Correção de upload

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mvn verify clean --fail-never -Dmaven.test.skip=true
+#mvn verify clean --fail-never -Dmaven.test.skip=true dependency:go-offline
+mvn dependency:go-offline
 mvn package -Dmaven.test.skip=true
 touch VERSION.txt
 echo $(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout) > VERSION.txt
--- src/main/java/org/lemanoman/videoviz/controller/UploadController.java
+++ src/main/java/org/lemanoman/videoviz/controller/UploadController.java
@@ -11,6 +11,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.lemanoman.videoviz.Constants;
 import org.lemanoman.videoviz.Resposta;
 import org.lemanoman.videoviz.Scrapper;
+import org.lemanoman.videoviz.Utils;
 import org.lemanoman.videoviz.dto.*;
 import org.lemanoman.videoviz.model.*;
 import org.lemanoman.videoviz.repositories.*;
@@ -77,7 +78,7 @@ public class UploadController {
                 LocationModel locationModel = giveLocation.get();
                 for (MultipartFile file : files) {
                     try {
-                        String code = getNextCode();
+                        String code = Utils.getRandomString(20);
                         StoreResult storeResult = videoFileService.storeVideo(locationModel.getPath(), code + ".mp4", file.getInputStream());
                         if (storeResult != null) {
                             VideoModel videoModel = new VideoModel();
